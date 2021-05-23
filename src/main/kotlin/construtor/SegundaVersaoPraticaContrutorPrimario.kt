17b219cd8b38cebf1463b45b7cb9bfdbc9562927
Ana Clara Guerra <ana.guerrads@hotmail.com>
2021-05-23 17:29:34 -0300

    Construtor Primário

---
+++ src/main/kotlin/construtor/SegundaVersaoPraticaContrutorPrimario.kt
@@ -0,0 +1,13 @@
+package construtor
+
+fun main() {
+    val primeiroCarro = ModeloCarro("Rosa", 143.0)
+    primeiroCarro.detalhesModeloCarro()
+}
+
+class ModeloCarro(val cor: String = "", var altura: Double = 0.0) {
+
+    fun detalhesModeloCarro() {
+        println("Cor: $cor, Altura: $altura")
+    }
+}
