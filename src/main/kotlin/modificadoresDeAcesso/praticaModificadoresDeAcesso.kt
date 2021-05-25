ca1f5e33f7bcd8146a0c3a5e822d46b966971db9
Ana Clara Guerra <ana.guerrads@hotmail.com>
2021-05-25 11:23:26 -0300

    Modificadores de Acesso

--- src/main/kotlin/modificadoresDeAcesso/praticaModificadoresDeAcesso.kt
+++ src/main/kotlin/modificadoresDeAcesso/praticaModificadoresDeAcesso.kt
@@ -1,2 +1,26 @@
 package modificadoresDeAcesso
 
+fun main() {
+    val primeiroCarro = Carro()
+    primeiroCarro.detalhes()
+    primeiroCarro.acelerar()
+    primeiroCarro.parar()
+    println()
+}
+
+open class Veiculo(
+    val cor: String = "Rosa"
+) {
+    fun acelerar() {
+        println("O veículo acelera.")
+    }
+    fun parar() {
+        println("O veículo para.")
+    }
+}
+
+class Carro: Veiculo() {
+    fun detalhes() {
+        println("Cor: $cor.")
+    }
+}
