c5d19b99bcdcb8f75af4f26311490c1015a94f5c
Ana Clara Guerra <ana.guerrads@hotmail.com>
2021-05-26 18:44:22 -0300

    Interface

---
+++ src/main/kotlin/abstracao/praticaAbstracao.kt
@@ -0,0 +1,5 @@
+package abstracao
+
+fun main() {
+
+}
--- src/main/kotlin/interface/praticaInterface.kt
+++ src/main/kotlin/interface/praticaInterface.kt
@@ -1,2 +1,43 @@
 package `interface`
 
+fun main() {
+    val limao = Limao()
+    limao.vitaminaC()
+    limao.vitaminaE()
+    println()
+
+    val jabuticaba = Jabuticaba()
+    jabuticaba.vitaminaC()
+    jabuticaba.vitaminaE()
+}
+
+interface Vitaminas {
+    fun vitaminaE()
+    fun vitaminaC()
+}
+
+open class Frutas() {
+    fun vitamina() {
+        println("Todas as frutas possuem vitamina")
+    }
+}
+
+class Limao: Frutas(), Vitaminas {
+    override fun vitaminaE() {
+        println("A fruta limão NÃO tem vitamina E!")
+    }
+
+    override fun vitaminaC() {
+        println("A fruta limão tem a vitamina C!")
+    }
+}
+
+class Jabuticaba: Frutas(), Vitaminas {
+    override fun vitaminaE() {
+        println("A fruta Jabuticaba tem a vitamina E.")
+    }
+    override fun vitaminaC() {
+        println("A fruta Jabuticaba NÃO tem a vitamina C!")
+    }
+
+}
